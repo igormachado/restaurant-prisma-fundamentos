73a12166eac4af10c96f759fc21cf281fad25233
igormachado <rf59imft@gmail.com>
2024-01-04 09:02:30 -0300

    git cache cleared

--- prisma/migrations/20220528121513_update_table_order/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "orders" DROP CONSTRAINT "orders_id_cooker_fkey";
-
--- AlterTable
-ALTER TABLE "orders" ALTER COLUMN "id_cooker" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "orders" ADD CONSTRAINT "orders_id_cooker_fkey" FOREIGN KEY ("id_cooker") REFERENCES "cooker"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- src/@types/express/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare namespace Express {
-  export interface Request {
-    id_cooker: string;
-    id_waiter: string;
-  }
-}
